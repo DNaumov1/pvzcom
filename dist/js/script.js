3070ad6bb605ae8d05e805128d676752307400f2
DNaumov1 <69724975+DNaumov1@users.noreply.github.com>
2022-05-22 18:46:33 +0300

    Add files via upload

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,7 @@ let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 let report = document.querySelector('.report');
 
-menuBtn.addEventListener('touchend', function(){
+menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
     report.classList.toggle('disable');
